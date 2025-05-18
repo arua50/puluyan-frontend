226bcf6cef3d75d729b7bb17bde82b136ab9a0b6
arua <aruawatt@gmail.com>
2025-05-18 23:14:31 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // suppress chunk size warning
+    chunkSizeWarningLimit: 2000, // Increase to suppress warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    }
   }
 })
