e1e356e1c997234b192cfc59f12f1b2e13a81681
arua <aruawatt@gmail.com>
2025-08-19 07:33:48 +0800

    3dmodel

--- src/pages/Artwork3DView.jsx
+++ src/pages/Artwork3DView.jsx
@@ -30,7 +30,7 @@ const Artwork3DView = () => {
     const fetchArtwork = async () => {
       try {
         const response = await fetch(
-          `https://puluyanartgallery.onrender.com/api/artworks?filters[documentId][$eq]=${id}&populate=*`
+          `https://puluyanartgallery.onrender.com/api/artworks?filters[exhibition][id][$eq]=${id}&populate=*`
         );
 
         if (!response.ok) throw new Error("Failed to fetch artwork");
